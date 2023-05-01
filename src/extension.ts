9850327cd1e94c033c25703d80c8b909084b5629
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-05-02 01:56:12 +0530

    PROD_RELEASE: 0.0.4

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,6 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 
-const LOGIN_URL = 'http://127.0.0.1:8000';
-const COOKIE_NAME = 'my-extension-token';
-
 function readFileContents(filePath: string): string {
 	try {
 		// Read file contents synchronously
@@ -22,37 +19,30 @@ function getWebviewContent() {
 
 const webviewContent = getWebviewContent();
 
+
+class MyWebviewViewProvider implements vscode.WebviewViewProvider {
+	resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext<unknown>,
+		token: vscode.CancellationToken
+	): void | Thenable<void> {
+		webviewView.webview.options = { enableScripts: true };
+		// Create the initial content of the webview
+		webviewView.webview.html = webviewContent;
+		// Listen for messages from the webview
+		webviewView.webview.onDidReceiveMessage((message) => {
+			if (message.command === 'sessionToken') {
+				console.log(process.cwd());
+			}
+		});
+	}
+}
+
+
 export function activate(context: vscode.ExtensionContext) {
-
-	let myWebviewViewProvider = {
-		resolveWebviewView(
-			webviewView: vscode.WebviewView,
-			context: vscode.WebviewViewResolveContext<unknown>,
-			token: vscode.CancellationToken
-		): void | Thenable<void> {
-			webviewView.webview.options = {enableScripts: true};
-	
-			// Create the initial content of the webview
-			webviewView.webview.html = webviewContent;
-			// Listen for messages from the webview
-			webviewView.webview.onDidReceiveMessage((message) => {
-				if (message.command === 'sessionToken') {
-					const sessionToken = message.value;
-					// Do something with the sessionToken value
-					console.log(`session_token: ${sessionToken}`);
-	
-					console.log(process.cwd());
-					// panel.webview.html = webviewContent;
-	
-				}
-			});
-		}
-	};
-
 	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider('extension.codemeWebview', myWebviewViewProvider)
+		vscode.window.registerWebviewViewProvider('extension.codemeWebview', new MyWebviewViewProvider())
 	);
-
 }
 
 export function deactivate() { }
